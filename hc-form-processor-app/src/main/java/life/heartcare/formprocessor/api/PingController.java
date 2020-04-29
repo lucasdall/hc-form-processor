8057a0df5006c096df0a364493ef56738c054416
lucas <lucas@lucas-ubuntu>
2020-04-29 19:50:51 -0300

    ping controller

--- hc-form-processor-app/src/main/java/life/heartcare/formprocessor/api/PingController.java
+++ hc-form-processor-app/src/main/java/life/heartcare/formprocessor/api/PingController.java
@@ -1,5 +1,13 @@
 package life.heartcare.formprocessor.api;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+import javax.annotation.PostConstruct;
+
+import org.apache.commons.lang3.time.DateFormatUtils;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,9 +16,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class PingController {
 
-	@GetMapping(path = "/ping", produces = MediaType.TEXT_HTML_VALUE)
-	public ResponseEntity<String> ping() throws Exception {
-		return ResponseEntity.ok("<b>running... 1</b>");
+	private Properties res = new Properties();
+	
+	@PostConstruct
+	public void setup() throws Exception {
+		res.load(new ClassPathResource("/version.properties").getInputStream());
+	}
+	
+	@GetMapping(path = "/ping", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Map<String, Object>> ping() throws Exception {
+		Map<String, Object> map = new HashMap<>();
+		map.put("running", "true");
+		map.put("version", res.getProperty("version"));
+		map.put("build-date", DateFormatUtils.ISO_8601_EXTENDED_DATETIME_TIME_ZONE_FORMAT.parse((String)res.getProperty("build.date")));
+		return ResponseEntity.ok(map);
 	}
 
 }
